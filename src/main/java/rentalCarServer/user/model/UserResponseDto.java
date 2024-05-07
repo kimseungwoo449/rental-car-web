ef1e12558c307e6974e4b3e902a6e1ab66839f78
kimseungwoo449 <ehdanf449@naver.com>
2024-05-07 17:02:09 +0900

    add : getter

--- src/main/java/rentalCarServer/user/model/UserResponseDto.java
+++ src/main/java/rentalCarServer/user/model/UserResponseDto.java
@@ -8,8 +8,7 @@ public class UserResponseDto {
 	private String phone;
 	private String gender;
 	private String name;
-	
-	
+
 	public UserResponseDto(String id, String password, String email, String residentNumber, String phone, String gender,
 			String name) {
 		super();
@@ -21,28 +20,61 @@ public class UserResponseDto {
 		this.gender = gender;
 		this.name = name;
 	}
-	
+
+	public String getId() {
+		return id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getResidentNumber() {
+		return residentNumber;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public void setResidentNumber(String residentNumber) {
 		this.residentNumber = residentNumber;
 	}
+
 	public void setPhone(String phone) {
 		this.phone = phone;
 	}
+
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	
+
 }
