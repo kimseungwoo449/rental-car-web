6534d8e49237721edefbc93ce4c9547499ce6ce2
kimseungwoo449 <ehdanf449@naver.com>
2024-05-10 11:07:49 +0900

    add : 생성자 및 getInstance

--- src/main/java/rentalCarServer/reservation/model/ReservationDao.java
+++ src/main/java/rentalCarServer/reservation/model/ReservationDao.java
@@ -1,5 +1,22 @@
 package rentalCarServer.reservation.model;
 
-public class ReservationDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+
+public class ReservationDao {
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+
+	private static ReservationDao instance = new ReservationDao();
+
+	private ReservationDao() {
+
+	}
+
+	public static ReservationDao getInstance() {
+		return instance;
+	}
 }
